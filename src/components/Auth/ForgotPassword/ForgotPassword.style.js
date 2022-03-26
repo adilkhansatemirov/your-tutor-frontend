4625cccc606adc4dfae81ed469b8610c32cef575
180103300 <180103300@stu.sdu.edu.kz>
2022-03-27 01:36:08 +0600

    signIn update

--- src/components/Auth/ForgotPassword/ForgotPassword.style.js
+++ src/components/Auth/ForgotPassword/ForgotPassword.style.js
@@ -6,13 +6,23 @@ export default makeStyles(() => ({
   },
   text: {
     marginBottom: 20,
-    color: '#343434',
+    color:'#E8EDF7',
   },
   input: {
     marginBottom: 30,
+    backgroundColor:'#E8EDF7',
   },
   title: {
     marginTop: 15,
     fontSize: 33,
+    color:'#E8EDF7',
+  },
+  button: {
+    minWidth: 'auto',
+    width: 100,
+    marginTop: 30,
+    backgroundColor:'#FFC739',
+    color:'#2F3D40',
+    borderRadius:'20px',
   },
 }));
